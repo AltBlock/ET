5396648af652f626e2d54951791fcf89083c792b
amarnepali <nepaliamar1998@gmail.com>
2018-07-20 10:10:23 +0545

    Add files via upload
    v0.1

---
+++ sketch_jul17a/functions.ino
@@ -0,0 +1,82 @@
+void manualCalibrate(){
+//manual calibration not complete 
+
+toAndroid(String("Calibration Starting..."));
+delay(1500);
+
+for(int i=1;i<=26;i++){
+
+toAndroid(String("Now Calibrating"));
+delay(1500);
+toAndroid(String(char('A'+i-1))); 
+
+int ttemp=millis();
+
+
+while(  ( millis()-ttemp)<2000 ){
+  for(int i=2;i<=6;i++){
+      if( digitalRead(i)==0){
+//        octr[i]++;
+        }
+     
+    }  
+}
+  
+  toAndroid("Calibrated");
+  delay(1000);
+  toAndroid(String(char('A'  +i))); 
+}
+
+}
+
+
+
+void PinAssign(){
+pinMode(A0,INPUT);   
+for(int i=2;i<=6;i++){
+      pinMode(i,INPUT);
+    }
+ pinMode(13,OUTPUT);
+ digitalWrite(13,HIGH);     
+}
+
+void toAndroid( const String& txt){
+  /*  String acc="";
+
+for(int i=0;i<txt.length();i++){
+
+if( txt[i]==' '){
+  Serial.println(acc);
+  delay(500);
+  acc="";
+}
+acc=acc+txt[i];
+}
+  Serial.println(acc); */
+  Serial.println(txt);
+  delay(2500);
+}
+
+
+void autoCalibrate(){
+  String input[]={"Hello","10000","How are you","01000","I am good","00110","What's your name","00111","Call me","01110","Love you too","00111","All the Best","01111","Where is the toilet","11110"};
+  int ctr=0;
+  for(int i=0;i<15;i+=2){
+  symbol[ctr].text=input[i];
+  symbol[ctr].vr=input[i+1];
+  ctr++;
+  }
+ 
+}
+
+void lookupsym(String& input){
+  for(int i=0;i<totsym;i++){
+    if(symbol[i].vr==input){
+      toAndroid(symbol[i].text);
+      return;
+      }  
+  }
+ toAndroid(String("Sorry No Symbol Match")); 
+ delay(1000);
+}  
+
---
+++ sketch_jul17a/sketch_jul17a.ino
@@ -0,0 +1,53 @@
+#include<Arduino.h>
+#include<String.h>
+#define totsym 8
+
+typedef struct symbol sym;
+void manualCalibrate();
+void autoCalibrate();
+void lookupsym(String& input);
+
+struct symbol{
+  String vr;  //voltage reading
+  String text;
+
+  symbol():text(""),vr(""){}
+};
+
+sym symbol [totsym];
+String input=String("00000");
+   
+void setup() {
+Serial.begin(9600);
+toAndroid(String("Now Starting..."));
+
+//manualCalibrate(symbol);
+autoCalibrate();
+//toAndroid(String("End of Calibration"));
+toAndroid(String("Waiting for the signal now "));
+delay(3500);
+}
+
+void loop() {
+     bool flag=0;
+  
+    lbl:
+    
+    for(int i=2;i<=6;i++){
+    input[i-2]= char( digitalRead(i)+'0' );
+    }
+    delay(500);
+  
+    if(input=="00000"){
+    if (flag!=1){ //toAndroid("Now in Sleep Mode Press Anything to begin");
+      }
+
+    delay(10);
+    flag=1;
+    goto lbl;  
+    }else{
+      flag=0;
+    }
+   
+    lookupsym(input);  
+}
